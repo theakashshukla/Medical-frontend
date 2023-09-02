b9f00715df7be49c7f7f92e09ec3c25bb9541e6f
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-09-02 23:28:39 +0530

    Partner Added

---
+++ header/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Partner from "./component/Partner.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Partner />
+    </div>
+  );
+}
+
+export default App;
